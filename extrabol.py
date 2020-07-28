696a133cf5f38b8d44399cdb7c881cad5ddaaff2
Ashley Villar <ashley@Ashleys-MacBook-Pro-4.local>
2020-07-28 19:57:14 -0400

    initial commit

---
+++ extrabol.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+def read_in_photometry(filename):
+    '''
+    Read in SN file
+    '''
+    return 0
+
+def interpolate(lc):
+    '''
+    Interpolate using 2D GP
+    '''
+    return 0
+
+
+def fit_bb(dense_lc):
+    '''
+    Fit each interpolate point to a BB, taking into account GP error
+    '''
+    return 0
+
+def make_outputs(bb_param):
+    '''
+    Make nice outputs
+    '''
