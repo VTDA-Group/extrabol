9074250a6ebd09233557950205d2a59b55a7f21f
Ashley Villar <ashley@Ashleys-MacBook-Pro-4.local>
2020-07-29 01:16:35 -0400

    fitting has a bug but im going to sleep

--- extrabol.py
+++ extrabol.py
@@ -4,9 +4,53 @@ import numpy as np
 from astroquery.svo_fps import SvoFps
 import matplotlib.pyplot as plt
 import george
-from scipy.optimize import minimize
+from scipy.optimize import minimize, curve_fit
 
 epsilon = 0.0001
+DM = 38.38 # HARD CODING CAUSE LAZY
+c = 3e10
+
+# STOLEN FROM MATT
+def bbody(lam,T,R):
+    '''
+    Calculate the corresponding blackbody radiance for a set
+    of wavelengths given a temperature and radiance.
+    Parameters
+    ---------------
+    lam: Reference wavelengths in Angstroms
+    T:   Temperature in Kelvin
+    R:   Radius in cm
+    Output
+    ---------------
+    Spectral radiance in units of erg/s/Angstrom
+    (calculation and constants checked by Sebastian Gomez)
+    '''
+
+    R = R * 1e15 #scale for code
+    T = T * 1e4 #scale for code
+
+    # Planck Constant in cm^2 * g / s
+    h = 6.62607E-27
+    # Speed of light in cm/s
+    c = 2.99792458E10
+
+    # Convert wavelength to cm
+    lam_cm = lam * 1E-8
+
+    # Boltzmann Constant in cm^2 * g / s^2 / K
+    k_B = 1.38064852E-16
+
+    # Calculate Radiance B_lam, in units of (erg / s) / cm ^ 2 / cm
+    exponential = (h * c) / (lam_cm * k_B * T)
+    B_lam = ((2. * np.pi * h * c ** 2) / (lam_cm ** 5)) / (np.exp(exponential) - 1)
+
+    # Multiply by the surface area
+    A = 4*np.pi*R**2
+
+    # Output radiance in units of (erg / s) / Angstrom
+    Radiance = B_lam * A / 1E8
+
+    return Radiance
 
 def read_in_photometry(filename):
     '''
@@ -32,7 +76,7 @@ def read_in_photometry(filename):
     zpts = []
     fluxes = []
     for datapoint in photometry_data:
-        mag = float(datapoint[1])
+        mag = float(datapoint[1]) - DM
         if datapoint[-1] == 'AB':
             zpts.append(3631.00)
         else:
@@ -99,11 +143,47 @@ def interpolate(lc):
     return dense_lc
 
 
-def fit_bb(dense_lc):
+def fit_bb(dense_lc,wvs):
     '''
     Fit each interpolate point to a BB, taking into account GP error
     '''
-    return 0
+
+    #STOLEN FROM MATT!!!
+    # Fit blackbody to SED (the one that is not padded with zeros)
+
+    T_arr = np.zeros(len(dense_lc))
+    R_arr = np.zeros(len(dense_lc))
+    for i,datapoint in enumerate(dense_lc):
+
+        fnu = 10.**((datapoint[:,0] + 48.60)/-2.5)
+        fnuplus = 10.**((datapoint[:,0] + datapoint[:,1] + 48.60)/-2.5)
+        fnu = fnu * 4. * np.pi * (3.086e22)**2 #LAZZYYYY assumption of 10 kpc
+        fnuplus = fnuplus * 4. * np.pi * (3.086e22)**2
+        flam = fnu * c / (wvs * 1e-8)**2
+        flamplus = fnuplus * c / (wvs * 1e-8)**2
+        ferr = np.abs(flam-flamplus)
+
+        BBparams, covar = curve_fit(bbody,wvs,flam,
+                            p0=(0.5,1),sigma=ferr)
+        # Get temperature and radius, with errors, from fit
+        T1 = BBparams[0]
+        T1_err = np.sqrt(np.diag(covar))[0]
+        R1 = np.abs(BBparams[1])
+        R1_err = np.sqrt(np.diag(covar))[1]
+
+        '''
+        
+        plt.plot(wvs,flam,'ko')
+        plt.plot(wvs,bbody(wvs,T1,R1),'ro')
+        plt.show()
+        '''
+        
+
+
+        T_arr[i] = T1
+        R_arr[i] = R1
+
+    return T_arr * 1e4,R_arr * 1e15
 
 def make_outputs(bb_param):
     '''
@@ -111,19 +191,19 @@ def make_outputs(bb_param):
     '''
 
 
-
-
 def main():
     lc,wv_corr,flux_corr = read_in_photometry('./example/Gaia16apd.dat')
     dense_lc = interpolate(lc)
     lc = lc.T
 
+    wvs = np.unique(lc[:,2]) * 1000.0 + wv_corr
+
     dense_lc[:,:,0] += flux_corr # This is now in AB mags
 
     #This code doesn't work, just test
-    colors = ['blue','red','green','yellow']
+    colors = ['blue','red','green','yellow','purple']
     gind = np.argsort(lc[:,0])
-    for jj in np.arange(4):
+    for jj in np.arange(5):
         plt.plot(lc[gind,0],dense_lc[gind,jj,0],color=colors[jj])
         plt.fill_between(lc[gind,0],dense_lc[gind,jj,0]-dense_lc[gind,jj,1],
                     dense_lc[gind,jj,0]+dense_lc[gind,jj,1],
@@ -133,6 +213,12 @@ def main():
     plt.title('Gaia16apd')
     plt.xlabel('MJD')
     plt.ylabel('Negative Mag')
+    plt.show()
+
+    Tarr,Rarr = fit_bb(dense_lc,wvs)
+    plt.plot(Tarr)
+    plt.show()
+    plt.plot(Rarr)
     plt.show()
 
 if __name__ == "__main__":
