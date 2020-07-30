e6cc1634b6dee12adc1cc4c63ce04b0ac3575cca
Ashley Villar <ashley@Ashleys-MacBook-Pro-4.local>
2020-07-29 22:36:58 -0400

    full working example

--- extrabol.py
+++ extrabol.py
@@ -8,7 +8,8 @@ from scipy.optimize import minimize, curve_fit
 
 epsilon = 0.0001
 DM = 38.38 # HARD CODING CAUSE LAZY
-c = 3e10
+c = 2.99792458E10
+sigsb = 5.6704e-5 #erg / cm^2 / s / K^4
 
 # STOLEN FROM MATT
 def bbody(lam,T,R):
@@ -26,13 +27,12 @@ def bbody(lam,T,R):
     (calculation and constants checked by Sebastian Gomez)
     '''
 
-    R = R * 1e15 #scale for code
-    T = T * 1e4 #scale for code
+    #R = R * 1e15 #scale for code
+    #T = T * 1e4 #scale for code
 
     # Planck Constant in cm^2 * g / s
     h = 6.62607E-27
-    # Speed of light in cm/s
-    c = 2.99792458E10
+
 
     # Convert wavelength to cm
     lam_cm = lam * 1E-8
@@ -42,13 +42,13 @@ def bbody(lam,T,R):
 
     # Calculate Radiance B_lam, in units of (erg / s) / cm ^ 2 / cm
     exponential = (h * c) / (lam_cm * k_B * T)
-    B_lam = ((2. * np.pi * h * c ** 2) / (lam_cm ** 5)) / (np.exp(exponential) - 1)
+    B_lam = ((2. * np.pi * h * c ** 2) / (lam_cm ** 5)) / (np.exp(exponential) - 1.)
 
     # Multiply by the surface area
-    A = 4*np.pi*R**2
+    A = 4. * np.pi * R**2
 
-    # Output radiance in units of (erg / s) / Angstrom
-    Radiance = B_lam * A / 1E8
+    # Output radiance in units of (erg / s) / cm
+    Radiance = B_lam * A
 
     return Radiance
 
@@ -134,6 +134,7 @@ def interpolate(lc):
         x_pred[jj*nfilts:jj*nfilts+nfilts, 1] = ufilts
 
     pred, pred_var = gp.predict(lc[:,1], x_pred, return_var=True)
+
     for jj in np.arange(nfilts):
         gind = np.where(np.abs(x_pred[:, 1]-ufilts[jj])<epsilon)[0]
         dense_fluxes[:, int(jj)] = pred[gind]
@@ -155,35 +156,42 @@ def fit_bb(dense_lc,wvs):
     R_arr = np.zeros(len(dense_lc))
     for i,datapoint in enumerate(dense_lc):
 
-        fnu = 10.**((datapoint[:,0] + 48.60)/-2.5)
-        fnuplus = 10.**((datapoint[:,0] + datapoint[:,1] + 48.60)/-2.5)
-        fnu = fnu * 4. * np.pi * (3.086e22)**2 #LAZZYYYY assumption of 10 kpc
-        fnuplus = fnuplus * 4. * np.pi * (3.086e22)**2
-        flam = fnu * c / (wvs * 1e-8)**2
+        fnu = 10.**((-datapoint[:,0] + 48.6)/-2.5)
+        fnuplus = 10.**((-datapoint[:,0] + datapoint[:,1] + 48.6)/-2.5) 
+        fnu = fnu * 4. * np.pi * (3.086e19)**2 #LAZZYYYY assumption of 10 kpc
+        fnuplus = fnuplus * 4. * np.pi * (3.086e19)**2
+
+        flam = fnu * c / (wvs * 1e-8)**2 
         flamplus = fnuplus * c / (wvs * 1e-8)**2
         ferr = np.abs(flam-flamplus)
 
-        BBparams, covar = curve_fit(bbody,wvs,flam,
-                            p0=(0.5,1),sigma=ferr)
-        # Get temperature and radius, with errors, from fit
-        T1 = BBparams[0]
-        T1_err = np.sqrt(np.diag(covar))[0]
-        R1 = np.abs(BBparams[1])
-        R1_err = np.sqrt(np.diag(covar))[1]
+        flam = flam 
+        ferr = ferr / 1e87
 
-        '''
+        try:
+            BBparams, covar = curve_fit(bbody,wvs,flam,
+                                p0=(9000,1e15),sigma=ferr)
+            # Get temperature and radius, with errors, from fit
+            T1 = BBparams[0]
+            T1_err = np.sqrt(np.diag(covar))[0]
+            R1 = np.abs(BBparams[1])
+            R1_err = np.sqrt(np.diag(covar))[1]
+        except:
+            T1 = np.nan
+            R1 = np.nan
+
         
-        plt.plot(wvs,flam,'ko')
-        plt.plot(wvs,bbody(wvs,T1,R1),'ro')
-        plt.show()
-        '''
+        
+        plt.plot(wvs,flam,'k')
+        plt.plot(wvs,bbody(wvs,T1,R1),'r')
+        print(T1,R1)
         
 
 
         T_arr[i] = T1
         R_arr[i] = R1
-
-    return T_arr * 1e4,R_arr * 1e15
+    plt.show()
+    return T_arr,R_arr
 
 def make_outputs(bb_param):
     '''
@@ -198,28 +206,39 @@ def main():
 
     wvs = np.unique(lc[:,2]) * 1000.0 + wv_corr
 
+
     dense_lc[:,:,0] += flux_corr # This is now in AB mags
 
     #This code doesn't work, just test
-    colors = ['blue','red','green','yellow','purple']
+    colors = ['blue','red','green','yellow','purple','cyan']
     gind = np.argsort(lc[:,0])
-    for jj in np.arange(5):
+    for jj in np.arange(6):
         plt.plot(lc[gind,0],dense_lc[gind,jj,0],color=colors[jj])
         plt.fill_between(lc[gind,0],dense_lc[gind,jj,0]-dense_lc[gind,jj,1],
                     dense_lc[gind,jj,0]+dense_lc[gind,jj,1],
                     color=colors[jj],alpha=0.2)
 
-    plt.plot(lc[gind,0],lc[gind,1] + flux_corr,'ko')
+    for i,filt in enumerate(np.unique(lc[:,2])):
+        gind = np.where(lc[:,2]==filt)
+        plt.plot(lc[gind,0],lc[gind,1] + flux_corr,'o',color=colors[i])
     plt.title('Gaia16apd')
     plt.xlabel('MJD')
     plt.ylabel('Negative Mag')
     plt.show()
 
     Tarr,Rarr = fit_bb(dense_lc,wvs)
-    plt.plot(Tarr)
+    plt.plot(lc[:,0],Tarr,'o')
     plt.show()
-    plt.plot(Rarr)
+    plt.plot(lc[:,0],Rarr,'o')
     plt.show()
+
+    bol_lum = 4. * np.pi * Rarr **2 * sigsb * Tarr**4
+    plt.plot(lc[:,0],bol_lum,'o')
+    plt.title('Gaia16apd')
+    plt.xlabel('MJD')
+    plt.ylabel('Bolometric Luminosity')
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
