fd75abe4782a84e952d83f9db7406e47f6304095
Ashley Villar <ashley@ashleys-mbp-4.myfiosgateway.com>
2020-07-31 17:43:36 -0400

    updating for pypi

---
+++ README.md
@@ -0,0 +1,3 @@
+Get bolometric estimates of SNe
+
+Note: You need the absolute LATEST version of astroquery to work with SVO
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="extrabol", # Replace with your own username
+    version="0.0.2",
+    author="V. Ashley Villar",
+    author_email="vav2110@columbia.edu",
+    description="Estimate SN bolometric light curves",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/extrabol",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
