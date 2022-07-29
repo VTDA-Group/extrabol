1902c23964ff24289eaf715e4aa192cd7d80a112
Imax2001 <iot5037@psu.edu>
2022-07-29 11:00:25 -0400

    nvm

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     ],
     entry_points = {
             'console_scripts': [
-                    'extrabol = extrabol/extrabol:main',
+                    'extrabol = extrabol.extrabol:main',
                 ]},
     python_requires='>=3.6'
 )
