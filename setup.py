d0116c99996ab07c8015623bbed6b4deb3b90fd0
Imax2001 <iot5037@psu.edu>
2022-08-15 14:41:52 -0400

    test3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as readme_file:
 setuptools.setup(
     name="extrabol", # Replace with your own username
     version="0.0.20",
-    py_modules=['extrabol.extrabol', 'extrabol.extrabol.snana2extrabol'],
+    py_modules=['extrabol', 'snana2extrabol'],
     author="Ian M. Thornton",
     author_email="iot5037@psu.edu",
     description="Estimate SN bolometric light curves",
@@ -25,8 +25,8 @@ setuptools.setup(
     ],
     entry_points = {
             'console_scripts': [
-                    'extrabol = extrabol.extrabol',
-                    'snana2extrabol = extrabol.extrabol.snana2extrabol:convert'
+                    'extrabol = extrabol.extrabol:main',
+                    'snana2extrabol = extrabol.snana2extrabol:convert'
                 ]},
     python_requires='>=3.6'
 )
