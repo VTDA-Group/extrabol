2c5824ea3b1ae4fef928b188baa59c8169e40bde
Imax2001 <iot5037@psu.edu>
2022-08-16 18:04:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/villrv/extrabol",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
         "numpy",
         "astropy",
