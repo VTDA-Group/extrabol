35b736c77d6d5b43f17eba81e83fb8478fa79534
Imax2001 <iot5037@psu.edu>
2022-08-23 16:21:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as readme_file:
+with open(os.path.join(here, 'docs/README.md'), encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
 setuptools.setup(
